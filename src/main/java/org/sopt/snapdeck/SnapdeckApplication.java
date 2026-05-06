08d6e3661d0087801431af178030708a432e2dfd
gichanGim <k.kychan2003@gmail.com>
2026-05-06 23:35:16 +0900

    chore: initial project setup

---
+++ src/main/java/org/sopt/snapdeck/SnapdeckApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.snapdeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapdeckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnapdeckApplication.class, args);
+    }
+
+}
