22f4c9eaae21e4d1c0e123d6efa88d9b3ef15991
gichanGim <k.kychan2003@gmail.com>
2026-05-12 16:26:01 +0900

    feat: Deck 엔티티 추가

---
+++ src/main/java/org/sopt/snapdeck/domain/deck/entity/Deck.java
@@ -0,0 +1,20 @@
+package org.sopt.snapdeck.domain.deck.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.sopt.snapdeck.global.common.entity.BaseTimeEntity;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@Entity
+@Table(name = "decks")
+public class Deck extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "visible_count", nullable = false)
+    private int visibleCount;
+}
