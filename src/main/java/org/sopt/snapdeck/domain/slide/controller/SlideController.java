8b16abf351d4d8b9f9a11acb12ac1c7dee06c133
1siis1 <sisees01@gmail.com>
2026-05-13 23:56:32 +0900

    feat: 슬라이드 순서 변경 controller 계층 코드 구현

--- src/main/java/org/sopt/snapdeck/domain/slide/controller/SlideController.java
+++ src/main/java/org/sopt/snapdeck/domain/slide/controller/SlideController.java
@@ -1,10 +1,17 @@
 package org.sopt.snapdeck.domain.slide.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.sopt.snapdeck.domain.slide.code.SlideSuccessCode;
+import org.sopt.snapdeck.domain.slide.dto.request.SlideOrderUpdateRequest;
+import org.sopt.snapdeck.domain.slide.dto.response.SlideResponse;
 import org.sopt.snapdeck.domain.slide.service.SlideService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.sopt.snapdeck.global.common.response.SuccessResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Slides", description = "Slides 관련 API")
 @RestController
@@ -12,4 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class SlideController {
     private final SlideService slideService;
+
+    @Operation(
+            summary = "슬라이드 순서 변경",
+            description = "슬라이드의 순서를 변경합니다."
+    )
+    @PatchMapping("/slides/{slideId}/order")
+    public ResponseEntity<SuccessResponse<List<SlideResponse>>>  updateSlideOrder(
+            @PathVariable Long slideId,
+            @RequestBody SlideOrderUpdateRequest request
+    ) {
+        List<SlideResponse> response = slideService.updateSlideOrder(slideId, request.toOrder());
+
+        return ResponseEntity.ok(
+                SuccessResponse.of(SlideSuccessCode.SLIDE_ORDER_CHANGED, response)
+        );
+    }
 }
