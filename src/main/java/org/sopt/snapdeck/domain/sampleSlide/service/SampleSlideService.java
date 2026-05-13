332e35747b9228ae63ff3a0e76d08bd7f15cab65
1siis1 <sisees01@gmail.com>
2026-05-13 21:41:15 +0900

    feat: sample-slides 서비스 계층 조회 기능 구현

--- src/main/java/org/sopt/snapdeck/domain/sampleSlide/service/SampleSlideService.java
+++ src/main/java/org/sopt/snapdeck/domain/sampleSlide/service/SampleSlideService.java
@@ -1,11 +1,23 @@
 package org.sopt.snapdeck.domain.sampleSlide.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.snapdeck.domain.sampleSlide.dto.response.SampleSlideResponse;
 import org.sopt.snapdeck.domain.sampleSlide.repository.SampleSlideRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SampleSlideService {
     private final SampleSlideRepository sampleSlideRepository;
+
+    @Transactional(readOnly = true)
+    public List<SampleSlideResponse> getSampleSlides(){
+
+        return sampleSlideRepository.findAll()
+                .stream().map(SampleSlideResponse::from).toList();
+
+    }
 }
