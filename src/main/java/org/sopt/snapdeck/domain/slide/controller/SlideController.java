5581d48881368396ac42586acc832e75202fe395
1siis1 <sisees01@gmail.com>
2026-05-15 00:42:09 +0900

    chore: request 매핑 정리

--- src/main/java/org/sopt/snapdeck/domain/slide/controller/SlideController.java
+++ src/main/java/org/sopt/snapdeck/domain/slide/controller/SlideController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Tag(name = "Slides", description = "Slides 관련 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/slides")
 public class SlideController {
     private final SlideService slideService;
 
@@ -32,7 +32,7 @@ public class SlideController {
             @ApiResponse(responseCode = "400", description = "슬라이드 변경 실패 - 같은 덱 슬라이드의 범위를 벗어난 순서 변경을 요청한 경우"),
             @ApiResponse(responseCode = "404", description = "슬라이드를 찾을 수 없음 — 존재하지 않는 slideId로 요청한 경우")
     })
-    @PatchMapping("/slides/{slideId}/order")
+    @PatchMapping("/{slideId}/order")
     public ResponseEntity<SuccessResponse<List<SlideResponse>>>  updateSlideOrder(
             @Parameter(description = "순서를 변경할 슬라이드 id", example = "1")
             @PathVariable Long slideId,
@@ -54,7 +54,7 @@ public class SlideController {
             @ApiResponse(responseCode = "200", description = "삭제 성공"),
             @ApiResponse(responseCode = "404", description = "슬라이드를 찾을 수 없음 — 존재하지 않는 slideId로 요청한 경우")
     })
-    @DeleteMapping("/slides/{slideId}")
+    @DeleteMapping("/{slideId}")
     public ResponseEntity<SuccessResponse<Void>> deleteSlide(
             @Parameter(description = "삭제할 슬라이드 id", example = "3")
             @PathVariable Long slideId
