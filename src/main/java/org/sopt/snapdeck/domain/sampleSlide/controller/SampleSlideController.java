5170f1de162b183e00275949692707f0e0bf93d8
1siis1 <sisees01@gmail.com>
2026-05-15 00:35:46 +0900

    chore: import 문 정리

--- src/main/java/org/sopt/snapdeck/domain/sampleSlide/controller/SampleSlideController.java
+++ src/main/java/org/sopt/snapdeck/domain/sampleSlide/controller/SampleSlideController.java
@@ -2,7 +2,6 @@ package org.sopt.snapdeck.domain.sampleSlide.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.sopt.snapdeck.domain.sampleSlide.code.SampleSlideSuccessCode;
@@ -10,7 +9,6 @@ import org.sopt.snapdeck.domain.sampleSlide.dto.response.SampleSlideResponse;
 import org.sopt.snapdeck.domain.sampleSlide.service.SampleSlideService;
 import org.sopt.snapdeck.global.common.response.SuccessResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
