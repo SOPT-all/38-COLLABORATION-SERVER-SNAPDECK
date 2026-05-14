293e634830a94ec346fcdfab28c464fd6a01ead3
gichanGim <k.kychan2003@gmail.com>
2026-05-14 22:29:52 +0900

    feat: deckId로 Chat 조회 메서드 작성

--- src/main/java/org/sopt/snapdeck/domain/chat/repository/ChatRepository.java
+++ src/main/java/org/sopt/snapdeck/domain/chat/repository/ChatRepository.java
@@ -4,6 +4,9 @@ import org.sopt.snapdeck.domain.chat.entity.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatRepository extends JpaRepository<Chat,Long> {
+    List<Chat> findAllByDeckIdOrderByOrderAsc(Long deckId);
 }
