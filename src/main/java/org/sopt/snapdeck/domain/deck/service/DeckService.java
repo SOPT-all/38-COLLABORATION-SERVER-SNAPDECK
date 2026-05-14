43120c823a3e617ffd6c6117d5ef3cffec3b23a0
gichanGim <k.kychan2003@gmail.com>
2026-05-14 21:23:59 +0900

    feat: getDeckSlide 메서드 작성

--- src/main/java/org/sopt/snapdeck/domain/deck/service/DeckService.java
+++ src/main/java/org/sopt/snapdeck/domain/deck/service/DeckService.java
@@ -1,11 +1,31 @@
 package org.sopt.snapdeck.domain.deck.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.snapdeck.domain.deck.code.DeckErrorCode;
+import org.sopt.snapdeck.domain.deck.dto.response.DeckSlideResponse;
+import org.sopt.snapdeck.domain.deck.entity.Deck;
 import org.sopt.snapdeck.domain.deck.repository.DeckRepository;
+import org.sopt.snapdeck.domain.slide.repository.SlideRepository;
+import org.sopt.snapdeck.global.exception.CustomException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DeckService {
     private final DeckRepository deckRepository;
+    private final SlideRepository slideRepository;
+
+    @Transactional(readOnly = true)
+    public List<DeckSlideResponse> getDeckSlides(Long deckId) {
+        if (!deckRepository.existsById(deckId))
+            throw new CustomException(DeckErrorCode.DECK_NOT_FOUND);
+
+        return slideRepository.findAllByDeckIdOrderByOrderAsc(deckId)
+                .stream()
+                .map(DeckSlideResponse::from)
+                .toList();
+    }
 }
