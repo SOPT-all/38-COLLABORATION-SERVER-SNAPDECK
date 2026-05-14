bc68755bc918fc78c25995be0a56a6a61bf972b7
gichanGim <k.kychan2003@gmail.com>
2026-05-14 21:24:45 +0900

    feat: deckId로 슬라이드 조회 메서드 작성

--- src/main/java/org/sopt/snapdeck/domain/slide/repository/SlideRepository.java
+++ src/main/java/org/sopt/snapdeck/domain/slide/repository/SlideRepository.java
@@ -4,6 +4,9 @@ import org.sopt.snapdeck.domain.slide.entity.Slide;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SlideRepository extends JpaRepository<Slide, Long> {
+    List<Slide> findAllByDeckIdOrderByOrderAsc(Long deckId);
 }
