8cf1f7679e41655c9beb5821a55fa39ed88526d5
gichanGim <k.kychan2003@gmail.com>
2026-05-14 23:38:30 +0900

    refactor: findAllByDeckIdOrderByOrderAsc 작성

--- src/main/java/org/sopt/snapdeck/domain/slide/repository/SlideRepository.java
+++ src/main/java/org/sopt/snapdeck/domain/slide/repository/SlideRepository.java
@@ -1,20 +1,13 @@
 package org.sopt.snapdeck.domain.slide.repository;
 
 import org.sopt.snapdeck.domain.slide.entity.Slide;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SlideRepository extends JpaRepository<Slide, Long> {
-    @Query("""
-    select s
-    from Slide s
-    join fetch s.deck
-    where s.deck.id = :deckId
-    order by s.order asc
-    """)
-    List<Slide> findAllByDeckIdWithDeck(Long deckId);
+    List<Slide> findAllByDeckIdOrderByOrderAsc(Long deckId, Pageable pageable);
 }
